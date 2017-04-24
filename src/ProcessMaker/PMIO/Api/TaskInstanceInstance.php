17a42d98b7c09ea1cf4cbcb339c11052521ac4df
marcoaurelio8 <marco.avalos@processmaker.com>
2017-04-24 15:57:06 -0400

    + Add TaskInstanceInstance

--- src/ProcessMaker/PMIO/Api/TaskInstanceInstance.php
+++ src/ProcessMaker/PMIO/Api/TaskInstanceInstance.php
@@ -21,4 +21,71 @@ class TaskInstanceInstance extends ApiPackage
         $settings[CURLOPT_HTTPHEADER][] = "Authorization: Bearer " . $this->api->getAccessToken();
         return $this->api->call($settings);
     }
+    public function store($instance, $task_id, $instance_id, $user_id, $group_id, $status, $priority, $delegate_date, $start_date, $finish_date, $task_due_date, $risk_date, $data, $duration, $instance_overdue_percentage)
+    {
+        $settings = $this->api->getSettings();
+        $settings[CURLOPT_CUSTOMREQUEST] = "POST";
+        $settings[CURLOPT_URL] = $this->api->getBaseUrl() . '/' . 'api/v1/instances/' . $instance . '/task_instances';
+        $settings[CURLOPT_HTTPHEADER][] = "Authorization: Bearer " . $this->api->getAccessToken();
+        $settings[CURLOPT_POSTFIELDS] = json_encode([
+            "data" => [
+                "type" => "task_instance",
+                "attributes" => $this->cleanData([
+                    "task_id" => $task_id,
+                    "instance_id" => $instance_id,
+                    "user_id" => $user_id,
+                    "group_id" => $group_id,
+                    "status" => $status,
+                    "priority" => $priority,
+                    "delegate_date" => $delegate_date,
+                    "start_date" => $start_date,
+                    "finish_date" => $finish_date,
+                    "task_due_date" => $task_due_date,
+                    "risk_date" => $risk_date,
+                    "data" => $data,
+                    "duration" => $duration,
+                    "instance_overdue_percentage" => $instance_overdue_percentage,
+                ])
+            ]
+        ]);
+        return $this->api->call($settings);
+    }
+    public function update($instance, $task_instance, $id, $task_id, $instance_id, $user_id, $group_id, $status, $priority, $delegate_date, $start_date, $finish_date, $task_due_date, $risk_date, $data, $duration, $instance_overdue_percentage)
+    {
+        $settings = $this->api->getSettings();
+        $settings[CURLOPT_CUSTOMREQUEST] = "PUT";
+        $settings[CURLOPT_URL] = $this->api->getBaseUrl() . '/' . 'api/v1/instances/' . $instance . '/task_instances/' . $task_instance ;
+        $settings[CURLOPT_HTTPHEADER][] = "Authorization: Bearer " . $this->api->getAccessToken();
+        $settings[CURLOPT_POSTFIELDS] = json_encode([
+            "data" => [
+                "type" => "task_instance",
+                "id" => $id,
+                "attributes" => $this->cleanData([
+                    "task_id" => $task_id,
+                    "instance_id" => $instance_id,
+                    "user_id" => $user_id,
+                    "group_id" => $group_id,
+                    "status" => $status,
+                    "priority" => $priority,
+                    "delegate_date" => $delegate_date,
+                    "start_date" => $start_date,
+                    "finish_date" => $finish_date,
+                    "task_due_date" => $task_due_date,
+                    "risk_date" => $risk_date,
+                    "data" => $data,
+                    "duration" => $duration,
+                    "instance_overdue_percentage" => $instance_overdue_percentage,
+                ])
+            ]
+        ]);
+        return $this->api->call($settings);
+    }
+    public function destroy($instance, $task_instance)
+    {
+        $settings = $this->api->getSettings();
+        $settings[CURLOPT_CUSTOMREQUEST] = "DELETE";
+        $settings[CURLOPT_URL] = $this->api->getBaseUrl() . '/' . 'api/v1/instances/' . $instance . '/task_instances/' . $task_instance ;
+        $settings[CURLOPT_HTTPHEADER][] = "Authorization: Bearer " . $this->api->getAccessToken();
+        return $this->api->call($settings);
+    }
 }
